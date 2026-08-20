dd1f0d3287cdfb9f743065394ded3c1603fd7fc5
Elisa Rubeo <rubeo.elisa4@gmail.com>
2026-08-20 19:39:49 +0200

    start

---
+++ src/main/java/it/uniroma3/siw/siw_festival/SiwFestivalApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.siw_festival;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwFestivalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwFestivalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/uniroma3/siw/siw_festival/SiwFestivalApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.siw_festival;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiwFestivalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
